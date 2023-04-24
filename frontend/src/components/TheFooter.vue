396918cc71adb6671fd47a72c964de1a8ea5a042
Michael Gee <mgee@bayviewglen.ca>
2023-04-24 14:39:46 -0400

    OK STEFANO HERES THE BLACK TEXT

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,9 +1,48 @@
 <template>
-        <footer class="footer">
-        <div class="content has-text-centered">
-            <p>
-                <strong>Equity Lab</strong><br>Copyright <a href="#">2023</a>
-            </p>
+    <footer class="footer">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <h1>&copy; Equity Lab 2023</h1>
+                    <p>To learn how we use your data to train our Artificial Intelligence technology, read our <a href="https://en.wikipedia.org/wiki/Privacy_policy" target="_blank">Privacy Policy</a></p>
+                </div>
+                <div class="column">
+                    <h1>Our Mission</h1>
+                    <ul>
+                        <li><a href="#">Landing</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="column">
+                    <h1>Get Studying</h1>
+                    <ul>
+                        <li><a href="#">Sign in / Log in</a></li>
+                        <li><a href="#">Study</a></li>
+                        <li><a href="#">Games</a></li>
+                    </ul>
+                </div>
+                <div class="column"></div>
+            </div>
+            <div class="social-bar">
+                <ul class="socials">
+                    <li>
+                        <a href="https://facebook.com"><i class="fa-brands fa-facebook-f fa-xl"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com"><i class="fa-brands fa-twitter fa-xl"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com"><i class="fa-brands fa-instagram fa-xl"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://discord.com"><i class="fa-brands fa-discord fa-xl"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://web.dev/googleplus"><i class="fa-brands fa-google-plus-g fa-xl"></i></a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </footer>
 </template>

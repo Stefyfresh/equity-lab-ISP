2db45fe987d545104659d8bb3274387747b5a90e
Michael Gee <mgee@bayviewglen.ca>
2023-04-26 09:18:11 -0400

    THE FOOTER

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -2,46 +2,54 @@
     <footer class="footer">
         <div class="container">
             <div class="columns">
-                <div class="column">
-                    <h1>&copy; Equity Lab 2023</h1>
-                    <p>To learn how we use your data to train our Artificial Intelligence technology, read our <a href="https://en.wikipedia.org/wiki/Privacy_policy" target="_blank">Privacy Policy</a></p>
+                <div class="column is-two-fifths has-text-left">
+                    <div class="block">&copy; Equity Lab 2023</div>
+                    <p class="block">We are devoted to bringing equitable teaching resources to the classroom.
+                        </p>
+                        <p>
+                        To learn how we use your personal data to train our Artificial Intelligence technology, read our <a
+                            href="https://en.wikipedia.org/wiki/Privacy_policy" target="_blank">Privacy Policy.</a></p>
                 </div>
-                <div class="column">
-                    <h1>Our Mission</h1>
+                <div class="column"></div>
+                <div class="column has-text-left">
+                    <div class="block">Our Mission</div>
                     <ul>
                         <li><a href="#">Landing</a></li>
                         <li><a href="#">About Us</a></li>
                         <li><a href="#">Contact Us</a></li>
                     </ul>
                 </div>
-                <div class="column">
-                    <h1>Get Studying</h1>
+                <div class="column has-text-left">
+                    <div class="block">Get Studying</div>
                     <ul>
-                        <li><a href="#">Sign in / Log in</a></li>
+                        <li><a href="#">Login</a></li>
                         <li><a href="#">Study</a></li>
                         <li><a href="#">Games</a></li>
                     </ul>
                 </div>
-                <div class="column"></div>
             </div>
             <div class="social-bar">
-                <ul class="socials">
-                    <li>
-                        <a href="https://facebook.com"><i class="fa-brands fa-facebook-f fa-xl"></i></a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com"><i class="fa-brands fa-twitter fa-xl"></i></a>
-                    </li>
-                    <li>
-                        <a href="https://instagram.com"><i class="fa-brands fa-instagram fa-xl"></i></a>
-                    </li>
-                    <li>
-                        <a href="https://discord.com"><i class="fa-brands fa-discord fa-xl"></i></a>
-                    </li>
-                    <li>
-                        <a href="https://web.dev/googleplus"><i class="fa-brands fa-google-plus-g fa-xl"></i></a>
-                    </li>
-                </ul>
+                <div class="columns">
+                    <div class="column is-one-fifth is-offset-three-fifths">
+                        <div class="columns">
+                            <div class="column">
+                                <a href="https://facebook.com"><i class="fa-brands fa-facebook-f fa-xl"></i></a>
+                            </div>
+                            <div class="column">
+                                <a href="https://twitter.com"><i class="fa-brands fa-twitter fa-xl"></i></a>
+                            </div>
+                            <div class="column">
+                                <a href="https://instagram.com"><i class="fa-brands fa-instagram fa-xl"></i></a>
+                            </div>
+                            <div class="column">
+                                <a href="https://discord.com"><i class="fa-brands fa-discord fa-xl"></i></a>
+                            </div>
+                            <div class="column">
+                                <a href="https://web.dev/googleplus"><i class="fa-brands fa-google-plus-g fa-xl"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>

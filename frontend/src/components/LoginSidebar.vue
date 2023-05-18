b9130bacc6ea041917473374c615a3df48cf1f7b
Michael Gee <mgee@bayviewglen.ca>
2023-05-18 09:46:44 -0400

    Login sort of better...

--- frontend/src/components/LoginSidebar.vue
+++ frontend/src/components/LoginSidebar.vue
@@ -6,104 +6,33 @@
                     src="https://images.pexels.com/photos/4709293/pexels-photo-4709293.jpeg?auto=compress&cs=tinysrgb&w=800">
             </figure>
         </div>
-        
+
         <div class="column is-6">
             <div class="column is-10 is-offset-1">
-            <div class="box profile-sidebar">
-                <aside class="menu is-hidden-mobile">
+                <div class="profile-sidebar">
+                    <aside class="menu is-hidden-mobile">
 
-                    <!-- TODO: Make this look waaaaay better -->
-                    <div class="field">
-                        <label class="label">Log In</label>
-                        <button class="button" @click="handleLogin">Student Login</button>
-                    </div>
-                    <!-- <div class="field">
-                        <label class="label">Username</label>
-                        <div class="control">
-                            <input class="input" type="text" placeholder="Text input">
+                        <!-- TODO: Make this look waaaaay better -->
+                        <div class="field">
+                            <label class="label">Log in to Equity Lab</label>
+                            <button class="button is-danger is-large is-fullwidth block"
+                                @click="handleLogin">
+                                    <span class="icon is-small">
+                                    <i class="fa-solid fa-graduation-cap"></i>
+                                    </span>
+                                    <span>Students</span>
+                            </button>
+                            <button class="button is-danger is-large is-fullwidth is-outlined"
+                                @click="handleLogin">
+                                    <span class="icon is-small">
+                                        <i class="fa-solid fa-chalkboard"></i>
+                                    </span>
+                                    <span>Teachers</span>
+                            </button>
                         </div>
-                    </div>
-
-                    <div class="field">
-                        <label class="label">Username</label>
-                        <div class="control has-icons-left has-icons-right">
-                            <input class="input is-success" type="text" placeholder="Text input" value="bulma">
-                            <span class="icon is-small is-left">
-                                <i class="fas fa-user"></i>
-                            </span>
-                            <span class="icon is-small is-right">
-                                <i class="fas fa-check"></i>
-                            </span>
-                        </div>
-                        <p class="help is-success">This username is available</p>
-                    </div>
-
-                    <div class="field">
-                        <label class="label">Email</label>
-                        <div class="control has-icons-left has-icons-right">
-                            <input class="input is-danger" type="email" placeholder="Email input" value="hello@">
-                            <span class="icon is-small is-left">
-                                <i class="fas fa-envelope"></i>
-                            </span>
-                            <span class="icon is-small is-right">
-                                <i class="fas fa-exclamation-triangle"></i>
-                            </span>
-                        </div>
-                        <p class="help is-danger">This email is invalid</p>
-                    </div>
-
-                    <div class="field">
-                        <label class="label">Subject</label>
-                        <div class="control">
-                            <div class="select">
-                                <select>
-                                    <option>Select dropdown</option>
-                                    <option>With options</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <label class="label">Message</label>
-                        <div class="control">
-                            <textarea class="textarea" placeholder="Textarea"></textarea>
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <div class="control">
-                            <label class="checkbox">
-                                <input type="checkbox">
-                                I agree to the <a href="#">terms and conditions</a>
-                            </label>
-                        </div>
-                    </div>
-
-                    <div class="field">
-                        <div class="control">
-                            <label class="radio">
-                                <input type="radio" name="question">
-                                Yes
-                            </label>
-                            <label class="radio">
-                                <input type="radio" name="question">
-                                No
-                            </label>
-                        </div>
-                    </div>
-
-                    <div class="field is-grouped">
-                        <div class="control">
-                            <button class="button is-link">Submit</button>
-                        </div>
-                        <div class="control">
-                            <button class="button is-link is-light">Cancel</button>
-                        </div>
-                    </div> -->
-                </aside>
+                    </aside>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 </template>

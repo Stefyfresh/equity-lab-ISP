f33f7388e98c77e503ef2c3a8ac1b20a08b10806
Samantha Sedran <ssedran@bayviewglen.ca>
2023-05-21 23:05:34 -0400

    Done AboutUs Yay! Just need to replace placeholder

--- frontend/src/views/AboutUs.vue
+++ frontend/src/views/AboutUs.vue
@@ -4,77 +4,63 @@ import TheFooter from '@/components/TheFooter.vue';
 </script>
 
 <template>
-   <TheNavigation></TheNavigation>
     <header class="header">
-        <div class="container is-fullhd"> 
-        <figure class="image is-fullwidth">
-            <img src="/images/aboutus.jpg">
-        </figure>
-    </div>
-
-        <!--<section class="section is-medium">
-              
-            
-        </section>-->
+        <TheNavigation></TheNavigation>
     </header>
-    <section class="container">
-        <div class="columns">
-            <div class="column is-two-thirds">
-                <p>Hello</p>
+    <section id="showcase">
+        <div class="bg-image"></div>
+            <div class="container is-flex is-justify-content-center is-align-items-center">
+            <div class="circle m-6 p-4 is-flex is-align-items-center">
+                <div>
+                <h1 class="title is-flex is-justify-content-center has-text-white">About Us</h1>
+                <p class="p-3">Started in ICS4U. three people, one dream, blah blah blah Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quod dolor dolores, voluptas suscipit officia alias ex ut corporis nesciunt a amet optio nisi ullam odio voluptate. Voluptatem, ipsum hic?</p>
+                </div>
             </div>
+            </div>
+    </section>
+    <section class="container pt-5 pb-5">
+        <div class="columns">
             <div class="column">
-                <p>Hello 2</p>
+                <figure class="image is-128x128">
+                    <img src="https://bulma.io/images/placeholders/128x128.png">
+                </figure>
+            </div>
+            <div class="column is-two-thirds is-flex is-align-items-center">
+                <p>Replace with photo of three of us. blah blah blah Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam numquam similique, perspiciatis doloribus nobis sapiente temporibus magnam repellendus labore, in vero modi harum quaerat iusto velit, provident aut porro veniam.</p>
             </div>
         </div>
     </section>
-    <!--<section class="section is-small has-background-white has-text-centered">
-        <div class="title">What students think about Equity Lab</div>
-        <div class="columns">
-            <div class="column">
-                <div class="box has-text-centered">
-                    <figure class="image is-128x128">
-                        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                      </figure>  
-                    <h1 class="title is-5">"The games are fun."</h1>
-                    <h1 class="subtitle is-6">An anonymous student, Grade 6</h1>
-                </div>
-            </div>
-            <div class="column">
-                <div class="box has-text-centered">
-                    <figure class="image is-128x128">
-                        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                      </figure>  
-                    <h1 class="title is-5">"This is cool."</h1>
-                    <h1 class="subtitle is-6">An anonymous student, Grade 5</h1>
-                </div>
-            </div>
-            <div class="column">
-                <div class="box has-text-centered">
-                    <figure class="image is-128x128">
-                        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-                      </figure>  
-                    <h1 class="title is-5">"Wow this is good"</h1>
-                    <h1 class="subtitle is-6">An anonymous student, Grade 8</h1>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="section is-medium">
-        <div class="columns">
-            <div class="column has-text-right">
-                <h1 class="title">Bring equity to the classroom.</h1>
-            </div>
-            <div class="column">
-                <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
-            </div>
-        </div>
-    </section>-->
+    
     <TheFooter></TheFooter>
 </template>
 
 <style>
-    .container img{
-        max-height: 300px;
-    }
+#showcase{
+    min-height: 600px;
+    text-align: center; 
+}
+
+#showcase .container{
+    min-height: 600px;
+    max-width: 500px;
+}
+
+.circle {
+    border-radius: 200px;
+    height: 400px;
+    background-color: #FF6312;
+    color: #fff;
+    opacity: 0.9;
+}
+
+#showcase .bg-image{
+    position: absolute;
+    background:url('../../public/images/aboutus.jpg') no-repeat center;
+    background-size:cover;
+    width:100%;
+    height: 600px;
+    opacity: 0.4;
+    z-index: -1;
+}
 </style>
 

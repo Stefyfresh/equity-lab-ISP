99e8c7945e92edaf2c6adbfa451249267ac37539
Samantha Sedran <ssedran@bayviewglen.ca>
2023-05-23 20:27:26 -0400

    Starting Study Sets

---
+++ frontend/src/views/Study.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheNavigation from '@/components/TheNavigation.vue';
+import TheFooter from '@/components/TheFooter.vue';
+</script>
+
+<template>
+    <header class="header">
+        <TheNavigation></TheNavigation>
+    </header>
+    
+    
+    <TheFooter></TheFooter>
+</template>

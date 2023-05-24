2c67486acc6513990628e0e93ba8d1c96fdf92ae
Michael Gee <mgee@bayviewglen.ca>
2023-05-24 09:40:44 -0400

    1. changed redirects to home.vue 2. made home and profile

--- frontend/src/components/ProfileSidebar.vue
+++ frontend/src/components/ProfileSidebar.vue
@@ -1,80 +1,76 @@
 <template>
-    <div class="columns is-gapless">
-        <div class="column is-3">
-            <div class="box profile-sidebar">
-                <aside class="menu is-hidden-mobile">
-                    <div class="has-text-centered block">
-                        <RouterLink to="/">
-                            <picture>
-                                <source srcset="/images/logo-light.webp" media="(prefers-color-scheme: dark)"
-                                    class="equity-lab-logo">
-                                <img src="/images/logo-dark.webp" alt="" class="equity-lab-logo">
-                            </picture>
+    <div class="box profile-sidebar">
+        <aside class="menu is-hidden-mobile">
+            <div class="has-text-centered block">
+                <RouterLink to="/">
+                    <picture>
+                        <source srcset="/images/logo-light.webp" media="(prefers-color-scheme: dark)"
+                            class="equity-lab-logo">
+                        <img src="/images/logo-dark.webp" alt="" class="equity-lab-logo">
+                    </picture>
+                </RouterLink>
+            </div>
+            <div class="card-image has-text-centered block">
+                <figure class="image is-128x128 is-inline-block">
+                    <img class="is-rounded" alt="" src="/images/128x128.webp">
+                </figure>
+            </div>
+            <div class="menu-list">
+                <span class="navbar-item sidebar-item">
+                    <button class="button is-dark is-inverted is-medium">
+                        <RouterLink to="home">
+                            <span class="icon is-medium">
+                                <i class="fa-solid fa-house"></i>
+                            </span>
+                            <span>Dashboard</span>
                         </RouterLink>
-                    </div>
-                    <div class="card-image has-text-centered block">
-                        <figure class="image is-128x128 is-inline-block">
-                            <img class="is-rounded" alt="" src="/images/128x128.webp">
-                        </figure>
-                    </div>
-                    <div class="menu-list">
-                        <span class="navbar-item sidebar-item">
-                            <button class="button is-dark is-inverted is-medium">
-                                <RouterLink to="study">
-                                    <span class="icon is-medium">
-                                        <i class="fa-solid fa-house"></i>
-                                    </span>
-                                    <span>Dashboard</span>
-                                </RouterLink>
+                    </button>
+                </span>
+                <span class="navbar-item sidebar-item">
+                    <button class="button is-dark is-inverted is-medium">
+                        <RouterLink to="study">
+                            <span class="icon is-medium">
+                                <i class="fa-solid fa-ranking-star"></i>
+                            </span>
+                            <span>Leaderboard</span>
+                        </RouterLink>
+                    </button>
+                </span>
+                <span class="navbar-item sidebar-item">
+                    <button class="button is-dark is-inverted is-medium">
+                        <RouterLink to="profile">
+                            <span class="icon is-medium">
+                                <i class="fa-solid fa-user"></i>
+                            </span>
+                            <span>Profile</span>
+                        </RouterLink>
+                    </button>
+                </span>
+                <span class="navbar-item sidebar-item">
+                    <div class="dropdown is-hoverable">
+                        <div class="dropdown-trigger">
+                            <button class="button is-dark is-inverted is-medium" aria-haspopup="true"
+                                aria-controls="dropdown-menu">
+                                <span class="icon is-small">
+                                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                                </span>
+                                <span>More</span>
                             </button>
-                        </span>
-                        <span class="navbar-item sidebar-item">
-                            <button class="button is-dark is-inverted is-medium">
-                                <RouterLink to="study">
-                                    <span class="icon is-medium">
-                                        <i class="fa-solid fa-ranking-star"></i>
-                                    </span>
-                                    <span>Leaderboard</span>
-                                </RouterLink>
-                            </button>
-                        </span>
-                        <span class="navbar-item sidebar-item">
-                            <button class="button is-dark is-inverted is-medium">
-                                <RouterLink to="study">
-                                    <span class="icon is-medium">
-                                        <i class="fa-solid fa-user"></i>
-                                    </span>
-                                    <span>Profile</span>
-                                </RouterLink>
-                            </button>
-                        </span>
-                        <span class="navbar-item sidebar-item">
-                            <div class="dropdown is-hoverable">
-                                <div class="dropdown-trigger">
-                                    <button class="button is-dark is-inverted is-medium" aria-haspopup="true"
-                                        aria-controls="dropdown-menu">
-                                        <span class="icon is-small">
-                                            <i class="fas fa-angle-down" aria-hidden="true"></i>
-                                        </span>
-                                        <span>More</span>
-                                    </button>
-                                </div>
-                                <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-                                    <div class="dropdown-content">
-                                        <div class="dropdown-item">
-                                            <a class="dropdown-item">
-                                                Settings
-                                            </a>
-                                            <LogoutButton class="dropdown-item"></LogoutButton>
-                                        </div>
-                                    </div>
+                        </div>
+                        <div class="dropdown-menu" id="dropdown-menu4" role="menu">
+                            <div class="dropdown-content">
+                                <div class="dropdown-item">
+                                    <a class="dropdown-item">
+                                        Settings
+                                    </a>
+                                    <LogoutButton class="dropdown-item"></LogoutButton>
                                 </div>
                             </div>
-                        </span>
+                        </div>
                     </div>
-                </aside>
+                </span>
             </div>
-        </div>
+        </aside>
     </div>
 </template>
 

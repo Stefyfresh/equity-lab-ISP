2fe73655f4ad903f212417be9de4d481960fdc52
StefyfreshSchool <90330171+StefyfreshSchool@users.noreply.github.com>
2023-05-27 22:46:28 -0400

    Head and feet

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,64 +1,77 @@
 <template>
     <footer class="footer">
-        <div class="container">
+        <div class="container px-5">
+
             <div class="columns">
                 <div class="column is-two-fifths has-text-left">
                     <div class="block">&copy; Equity Lab 2023</div>
-                    <p class="block">We are devoted to bringing equitable teaching resources to the classroom.
-                        </p>
-                        <p>
-                        To learn how we use your personal data to train our Artificial Intelligence technology, read our <a
+                    <p class="block">We are devoted to bringing equitable teaching resources to the classroom.</p>
+                    <p> To learn how we use your personal data to train our Artificial Intelligence technology, read our <a
                             href="https://en.wikipedia.org/wiki/Privacy_policy" target="_blank">Privacy Policy.</a></p>
                 </div>
                 <div class="column"></div>
-                <div class="column has-text-left">
-                    <div class="block">Our Mission</div>
-                    <ul>
-                        <li>
-                            <RouterLink to="/">Landing</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="about">About Us</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="contact">Contact Us</RouterLink>
-                        </li>
-                    </ul>
-                </div>
-                <div class="column has-text-left">
-                    <div class="block">Get Studying</div>
-                    <ul>
-                        <li>
-                            <RouterLink to="login">Log In</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="study">Study</RouterLink>
-                        </li>
-                        <li>
-                            <RouterLink to="study">Games</RouterLink>
-                        </li>
-                    </ul>
+                <div class="column is-two-fifths">
+
+                    <div class="columns is-vertical">
+                        <div class="column">
+                            <div class="columns is-mobile">
+                                <div class="column has-text-left">
+                                    <div class="block">Our Mission</div>
+                                    <ul>
+                                        <li>
+                                            <RouterLink to="/">Landing</RouterLink>
+                                        </li>
+                                        <li>
+                                            <RouterLink to="about">About Us</RouterLink>
+                                        </li>
+                                        <li>
+                                            <RouterLink to="contact">Contact Us</RouterLink>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="column has-text-left">
+                                    <div class="block">Get Studying</div>
+                                    <ul>
+                                        <li>
+                                            <RouterLink to="login">Log In</RouterLink>
+                                        </li>
+                                        <li>
+                                            <RouterLink to="study">Study</RouterLink>
+                                        </li>
+                                        <li>
+                                            <RouterLink to="study">Games</RouterLink>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="column">
+                                <div class="columns">
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-            <div class="social-bar">
-                <div class="columns">
-                    <div class="column is-one-fifth is-offset-three-fifths">
-                        <div class="columns">
-                            <div class="column">
-                                <a href="https://facebook.com" aria-label="Facebook"><i class="fa-brands fa-facebook-f fa-xl"></i></a>
-                            </div>
-                            <div class="column">
-                                <a href="https://twitter.com" aria-label="Twitter"><i class="fa-brands fa-twitter fa-xl"></i></a>
-                            </div>
-                            <div class="column">
-                                <a href="https://instagram.com" aria-label="Instagram"><i class="fa-brands fa-instagram fa-xl"></i></a>
-                            </div>
-                            <div class="column">
-                                <a href="https://discord.com" aria-label="Discord"><i class="fa-brands fa-discord fa-xl"></i></a>
-                            </div>
-                            <div class="column">
-                                <a href="https://web.dev/googleplus" aria-label="Google Plus"><i class="fa-brands fa-google-plus-g fa-xl"></i></a>
-                            </div>
+            <div class="columns">
+                <div class="column"></div>
+                <div class="column is-two-fifths">
+                    <div class="columns is-mobile">
+                        <div class="column">
+                            <a href="https://facebook.com" aria-label="Facebook"><i class="fa-brands fa-facebook-f fa-xl"></i></a>
+                        </div>
+                        <div class="column">
+                            <a href="https://twitter.com" aria-label="Twitter"><i class="fa-brands fa-twitter fa-xl"></i></a>
+                        </div>
+                        <div class="column">
+                            <a href="https://instagram.com" aria-label="Instagram"><i class="fa-brands fa-instagram fa-xl"></i></a>
+                        </div>
+                        <div class="column">
+                            <a href="https://discord.com" aria-label="Discord"><i class="fa-brands fa-discord fa-xl"></i></a>
+                        </div>
+                        <div class="column">
+                            <a href="https://web.dev/googleplus" aria-label="Google Plus"><i class="fa-brands fa-google-plus-g fa-xl"></i></a>
                         </div>
                     </div>
                 </div>

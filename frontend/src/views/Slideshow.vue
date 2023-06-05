2620db37d05cc5a450c421e799fd75e0dea8c6b2
StefyfreshSchool <90330171+StefyfreshSchool@users.noreply.github.com>
2023-06-05 15:38:50 -0400

    Make work

--- frontend/src/views/Slideshow.vue
+++ frontend/src/views/Slideshow.vue
@@ -5,8 +5,8 @@ import { ref, onMounted } from 'vue'
 import StudySetInfo from '@/services/StudySetInfo.js'
 import LoadingAnimation from '@/components/LoadingAnimation.vue';
 
-const experts = ref(null)
-let expert = "";
+const experts = ref(null);
+let expert = ref(null);
 let currExpert = 1;
 let numExperts = 2;
 
@@ -26,48 +26,48 @@ onMounted(() => {
     })
 })
 
-document.getElementById("previous").addEventListener("click", btnPrevious);
-document.getElementById("next").addEventListener("click", btnNext);
 
 
-function showPage(expertID){
-onMounted(() => {
-  StudySetInfo.getExpert(expertID)
-    .then((response) => {
-      expert.value = response.data
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-})
+function showPage(expertID) {
+  onMounted(() => {
+    StudySetInfo.getExpert(expertID)
+      .then((response) => {
+        expert.value = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  })
   expert = ref(null);
 }
 
-function btnPrevious(){
-  if(currExpert > 1){
+function btnPrevious() {
+  if (currExpert > 1) {
     currExpert--;
     showPage(currExpert);
   }
 }
 
 // Functionality for next button
-function btnNext(){
-  if(currExpert < numExperts){
+function btnNext() {
+  if (currExpert < numExperts) {
     currExpert++;
     showPage(currExpert);
   }
 }
 
 
-function start(){
-    createPagination();
-    showPage(currPage);
-  }
+function start() {
+  createPagination();
+  showPage(currPage);
+}
 
-  function createPagination(){
-    document.getElementById("previous").style.display = 'inline';
-    document.getElementById("next").style.display = 'inline';
+function createPagination() {
+  document.querySelector("#previous").addEventListener("click", btnPrevious);
+  document.querySelector("#next").addEventListener("click", btnNext);
 
+  document.querySelector("#previous").style.display = 'inline';
+  document.querySelector("#next").style.display = 'inline';
 }
 
 
@@ -75,15 +75,13 @@ function start(){
 
 <template>
   <TheNavigation></TheNavigation>
-
-  <body onload="start();">
   <div v-if="expert">
     <h1 class="title is-flex is-justify-content-center m-4 mb-5">Study Sets</h1>
     <div class="container is-fluid is-flex is-flex-wrap-wrap is-justify-content-center">
-          <div class="set card m-1 mb-2">
-            <!--<img :src="`/images/${expert.image}`" :alt="expert.name">-->
-            <h2 class="is-flex is-justify-content-center pb-2 title is-6">{{ expert.name }}</h2>
-          </div>
+      <div class="set card m-1 mb-2">
+        <!--<img :src="`/images/${expert.image}`" :alt="expert.name">-->
+        <h2 class="is-flex is-justify-content-center pb-2 title is-6">{{ expert.name }}</h2>
+      </div>
     </div>
     <!--<div v-if="experts">
     <h1 class="title is-flex is-justify-content-center m-4 mb-5">Study Sets</h1>
@@ -96,15 +94,14 @@ function start(){
         
       </span>
     </div>-->
-    <nav class="pagination is-centered" role="navigation" aria-label="pagination">
-      <a class="pagination-previous button mb-3 mx-1" id="previous">Previous</a>
-      <a class="pagination-next button mb-3 mx-1" id="next">Next page</a>
-      <ul class="pagination-list mb-2" id="pagination">
-       
-      </ul></nav>
-    </div>
-</body>
+  </div>
+  <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+    <a class="pagination-previous button mb-3 mx-1" id="previous">Previous</a>
+    <a class="pagination-next button mb-3 mx-1" id="next">Next page</a>
+    <ul class="pagination-list mb-2" id="pagination">
 
-    <TheFooter class="mt-3"></TheFooter>
+    </ul>
+  </nav>
 
+  <TheFooter class="mt-3"></TheFooter>
 </template>

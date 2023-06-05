c04e78389e66a047d7bd9df1d2fc0f7818e026a2
Michael Gee <mgee@bayviewglen.ca>
2023-06-04 20:34:52 -0400

    FINISHED LANDING PAGE

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -7,11 +7,15 @@ import TheFooter from '@/components/TheFooter.vue';
    <TheNavigation></TheNavigation>
     <header class="header">
         <div class="columns is-gapless has-background-light">
-                <div class="column has-text-right has-background-light">
-                    <h1 class="title">Bring equity to the classroom.</h1>
+                <div class="column has-background-light">
+                    <div class="columns">
+                        <div class="column is-6 is-offset-one-quarter">
+                            <h1 class="landing-text">Bring equity to the classroom.</h1>
+                        </div>
+                    </div>
                 </div>
-                <div class="column">
-                    <img alt="" src="https://www.turnitin.com/img/http/aHR0cHM6Ly9tYXJrZXRpbmctdGlpLXN0YXRhbWljLWFzc2V0cy11cy13ZXN0LTIuczMtdXMtd2VzdC0yLmFtYXpvbmF3cy5jb20vZWRpdG9yaWFsL2ltYWdlcy9lbXBvd2VyLXN0dWRlbnRzLXRvLWRvLXRoZWlyLWJlc3Qtb3JpZ2luYWwtd29yay5wbmc=?s=719a47c4dd5cb62edb02ac398dcf4449">
+                <div class="column is-5">
+                    <img alt="" src="https://images.ctfassets.net/mrop88jh71hl/41XDlZOCYpwd1eNZjFBFnn/45e436216d515f9dbe41e789f68e9e6e/stem_kids.jpeg">
                 </div>
             </div>
     </header>
@@ -20,40 +24,46 @@ import TheFooter from '@/components/TheFooter.vue';
         <div class="columns">
             <div class="column">
                 <div class="box has-text-centered">
-                    <figure class="image is-128x128">
-                        <img alt="" class="is-rounded" src="/images/128x128.webp">
+                    <figure class="image is-inline-block block">
+                        <img alt="" class="" src="https://media.istockphoto.com/id/854169794/photo/elementary-student-going-back-to-school.jpg?s=612x612&w=0&k=20&c=APEN-3y-QUx-3oaNJe0zNCjgpveudmDS1IxIhU63vT0=">
                       </figure>  
-                    <h1 class="title is-5">"The games are fun."</h1>
-                    <h1 class="subtitle is-6">An anonymous student, Grade 6</h1>
+                    <h1 class="title is-5">"The games are fun. <br>I think this is really cool"</h1>
+                    <h1 class="subtitle is-6 pt-2">Jane, Grade 6</h1>
                 </div>
             </div>
             <div class="column">
                 <div class="box has-text-centered">
-                    <figure class="image is-128x128">
-                        <img alt="" class="is-rounded" src="/images/128x128.webp">
+                    <figure class="image is-inline-block block">
+                        <img alt="" class="" src="https://media.istockphoto.com/id/887302366/photo/portrait-of-male-teenage-student-in-uniform-outside-buildings.jpg?s=612x612&w=0&k=20&c=hoha_IGYlGKM57gqX0QPQdnBvnrJPtHLn8ErZ6QK2nY=">
                       </figure>  
-                    <h1 class="title is-5">"This is cool."</h1>
-                    <h1 class="subtitle is-6">An anonymous student, Grade 5</h1>
+                    <h1 class="title is-5 block">"This makes learning about <br>STEM way better."</h1>
+                    <h1 class="subtitle is-6 pt-2">John, Grade 8</h1>
                 </div>
             </div>
             <div class="column">
                 <div class="box has-text-centered">
-                    <figure class="image is-128x128">
-                        <img alt="" class="is-rounded" src="/images/128x128.webp">
+                    <figure class="image is-inline-block block">
+                        <img alt="" class="" src="https://t3.ftcdn.net/jpg/02/02/52/36/360_F_202523652_NqEMp50IkORLXlCmJXqgqRBLMW8xBHMP.jpg">
                       </figure>  
-                    <h1 class="title is-5">"Wow this is good"</h1>
-                    <h1 class="subtitle is-6">An anonymous student, Grade 8</h1>
+                    <h1 class="title is-5">"With Equity Lab, my class is more engaged than ever"</h1>
+                    <h1 class="subtitle is-6 pt-2">Ms. Teacher, 7th grade teacher</h1>
                 </div>
             </div>
         </div>
     </section>
-    <section class="section is-medium">
+    <section class="section">
         <div class="columns">
-            <div class="column has-text-right">
-                <h1 class="title">Learn how to incorporate Equity Lab with your school's curriculum</h1>
+            <div class="column is-4 is-offset-2">
+                <figure class="image is-inline-block">
+                    <img alt="" class="" src="https://st4.depositphotos.com/3261171/26578/i/600/depositphotos_265787618-stock-photo-teacher-standing-near-whiteboard-with.jpg">
+                </figure>
+                <RouterLink to="about" class="title">Incorporate Equity Lab within your school's curriculum</RouterLink>
             </div>
-            <div class="column">
-                <img alt="" class="is-rounded" src="/images/128x128.webp">
+            <div class="column is-4">
+                <figure class="image is-inline-block">
+                    <img alt="" class="" src="https://jsis.washington.edu/canada/wp-content/uploads/sites/20/2017/06/gibney_students-800x534.jpg">
+                </figure>
+                <RouterLink to="about" class="title">Read about our mission to bring equity to schools across Canada</RouterLink>
             </div>
         </div>
     </section>

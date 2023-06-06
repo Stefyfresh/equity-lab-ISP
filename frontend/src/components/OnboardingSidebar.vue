a611ce57fd73156c684a7743901f9f7809bad5e9
Michael Gee <mgee@bayviewglen.ca>
2023-06-05 21:52:02 -0400

    ONBOARDING HAS FRONTEND

--- frontend/src/components/OnboardingSidebar.vue
+++ frontend/src/components/OnboardingSidebar.vue
@@ -13,28 +13,28 @@ const { loginWithRedirect } = useAuth0();
         <div class="column is-6">
             <div class="container is-fluid">
                 <div class="column is-10 is-offset-1">
-                    <aside class="menu has-text-centered">
+                    <aside class="menu has-text-left">
                         <div class="field">
-                            <h1 class="title pt-6 mt-6">Select your account type</h1>
-                            <div class="columns is-desktop">
-                                <div class="column">
-                                    <button class="button is-danger log-in-button is-large is-rounded is-fullwidth block is-outlined"
-                                        @click="handleLogin">
-                                        <span class="icon is-small">
-                                            <i class="fa-solid fa-graduation-cap"></i>
-                                        </span>
-                                        <span>Students</span>
-                                    </button>
+                            <h1 class="title pt-6 mt-6">Welcome to Equity Lab</h1>
+                            <div class="field">
+                                <label class="label is-large mt-6 pt-6">Select your account type</label>
+                                <div class="control">
+                                <div class="select is-large is-rounded">
+                                    <select>
+                                        <option>Student</option>
+                                        <option>Teacher</option>
+                                    </select>
                                 </div>
-                                <div class="column">
-                                    <button class="button is-danger log-in-button is-large is-rounded is-fullwidth is-outlined"
-                                        @click="handleLogin">
-                                        <span class="icon is-small">
-                                            <i class="fa-solid fa-chalkboard"></i>
-                                        </span>
-                                        <span>Teachers</span>
-                                    </button>
-                                </div>
+                            </div>
+                            </div>
+                            <div class="field">
+                                <label class="label is-large mt-5">Name</label>
+                                <p class="control">
+                                    <input class="input is-rounded is-large" type="name" placeholder="What's your name?">
+                                </p>
+                            </div>
+                            <div class="field">
+                                <button class="mt-3 button is-danger is-large is-rounded is-right mt-6 is-fullwidth">Create account</button>
                             </div>
                         </div>
                     </aside>
@@ -46,7 +46,7 @@ const { loginWithRedirect } = useAuth0();
 
 <style scoped>
 .log-in-button {
-  height: 100px;
+    height: 100px;
 }
 
 .login-sidebar {

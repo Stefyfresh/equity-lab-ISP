f5be9446c676e466d6c9f4b65d3c69e838571fdc
Samantha Sedran <ssedran@bayviewglen.ca>
2023-06-05 21:15:58 -0400

    Tried to edit study sets

--- frontend/src/views/StudySets.vue
+++ frontend/src/views/StudySets.vue
@@ -6,6 +6,7 @@ import StudySetInfo from '@/services/StudySetInfo.js'
 import LoadingAnimation from '@/components/LoadingAnimation.vue';
 
 const subjects = ref(null)
+const numSubjects = 7;
 
 onMounted(() => {
   StudySetInfo.getSubjects()
@@ -15,7 +16,75 @@ onMounted(() => {
     .catch((error) => {
       console.log(error)
     })
+    initSearch();
 })
+
+function initSearch(){
+  document.getElementById("submit").addEventListener("click", checkError);
+  document.getElementById("allteam").addEventListener("click", startclear);
+}
+
+function checkError(){
+  let available = false;
+  const subject = ref(null);
+  for(let i=1; i < numSubjects; i++){
+    StudySetInfo.getSubject(i)
+    .then((response) => {
+      subject.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+    if(document.getElementById("searchterms").value == subject.name){
+      i=numSubjects;
+      available = true;
+    }
+  }
+  if(available){
+    subjects = subject;
+    errorButtonReverse();
+  }else{
+    errorButton("Sorry, that is not a study set that we have. Please try again!");
+  }
+    
+}
+
+//Adds styling to error message and makes it visible
+function errorButton(message){
+  let errormsgouter = document.querySelector('#errormsgouter');
+  errormsgouter.classList.add('message');
+  errormsgouter.classList.add('is-danger');
+  errormsgouter.classList.add('m-3');
+
+  let errormsg = document.querySelector('#errormsg');
+  errormsg.classList.add('message-body');
+
+  let deletebtn = document.querySelector('#delete');
+  deletebtn.classList.remove('hidden');
+
+  document.querySelector('#errormsg').textContent = message;
+
+}
+
+//takes aways styling to error message and makes it inivisible
+function errorButtonReverse(){
+  let errormsgouter = document.querySelector('#errormsgouter');
+  errormsgouter.classList.remove('message');
+  errormsgouter.classList.remove('is-danger');
+  errormsgouter.classList.remove('m-3');
+
+  let errormsg = document.querySelector('#errormsg');
+  errormsg.classList.remove('message-body');
+
+  let deletebtn = document.querySelector('#delete');
+  deletebtn.classList.add('hidden');
+
+  document.querySelector('#errormsg').textContent = '';
+
+}
+
+
 
 
 </script>
@@ -23,10 +92,22 @@ onMounted(() => {
 <template>
   <TheNavigation></TheNavigation>
 
+  <div class="is-flex is-justify-content-center mb-3">
+    <div class="search is-flex is-flex-direction-row is-align-items-center is-justify-content-center">
+        <input class="input m-1 mb-2 is-rounded search" id="searchterms" type="text" placeholder="Looking for a particular study set? Search here!">
+        <button class="button is-light m-2 mb-2 is-rounded" id="submit">Submit</button>
+        <button class="button is-light m-2 mb-2 is-rounded" id="clear">Clear</button>
+      </div>
+    </div>
+    <article id="errormsgouter" class="is-flex is-align-items-center is-flex-direction-row">
+      <div id="errormsg"></div>
+      <button class="delete mr-5 hidden" id="delete"></button>
+    </article>
+
   
   <div v-if="subjects">
-    <h1 class="title is-flex is-justify-content-center m-4 mb-5">Study Sets</h1>
-    <div class="container is-fluid is-flex is-flex-wrap-wrap is-justify-content-center">
+    <h1 class="title is-flex is-justify-content-center m-4 mb-2">Study Sets</h1>
+    <div class="container is-fluid is-flex is-flex-wrap-wrap is-justify-content-center pb-3">
       <!--is-flex is-justify-content-center"-->
       <span v-for="subject in subjects" :key="subject.id">
         <router-link :to="{ name: 'studysubject', params: { subjectID: subject.subjectID } }">
@@ -56,6 +137,10 @@ onMounted(() => {
   border-radius: 10px;
   width: 400px;
   height: 250px;
+}
+
+.search{
+  width: 60%;
 }
 
 </style>
